a96c89f13f381946a341987343ea2dd740252a6b
pedrobono <pedro.bono11@gmail.com>
2016-07-20 15:41:58 -0300

    Ticket #4 - Criar Login de Sistema
    Alterado o script de usuario pois o outro estava um pouco confuso

--- Scripts/Criacao/usuario.sql
+++ Scripts/Criacao/usuario.sql
@@ -1,9 +1,8 @@
-CREATE TABLE `arearestrita`.`usuario` (
-  `idUsuario` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `login` VARCHAR(20) NOT NULL,
-  `senha` VARCHAR(50) NOT NULL,
-  `validadeSenha` DATETIME NOT NULL,
-  `perfilUsuario` VARCHAR(1) NOT NULL DEFAULT 'F' COMMENT 'A = ADMIN\nF = USUARIO FINAL\nR = REVENDA\n',
-  PRIMARY KEY (`idUsuario`),
-  UNIQUE INDEX `idUsuario_UNIQUE` (`idUsuario` ASC))
+CREATE TABLE usuario (
+  idUsuario INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  login VARCHAR(20) NOT NULL,
+  senha VARCHAR(50) NOT NULL,
+  validadeSenha DATETIME NOT NULL,
+  perfilUsuario VARCHAR(1) NOT NULL DEFAULT 'F' COMMENT 'A = ADMIN\nF = USUARIO FINAL\nR = REVENDA\n',
+  PRIMARY KEY (idUsuario))
 COMMENT = 'Tabela responsavel pelos dados do usuario.';
